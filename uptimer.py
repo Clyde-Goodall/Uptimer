d9067e1cf71a52d5a0df00899ef90384beb84c40
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 13:47:58 -0500

    Detection working as intended

--- uptimer.py
+++ uptimer.py
@@ -1,7 +1,7 @@
 from win10toast import ToastNotifier as toast
-import socket, schedule, csv, time
+import socket, csv, time, schedule
 from timeit import default_timer as timer
-from datetime import date
+from datetime import datetime
 
 #Entire uptimer class
 class Uptimer:
@@ -35,16 +35,6 @@ class Uptimer:
             host = socket.gethostbyname('1.1.1.1')
             sock = socket.create_connection((host, 80), 2)   
             print("Ping successful")
-            if(self.disconnected == True):
-                # self.recording = False
-                self.disconnected = False
-                
-                self.time_of_outage = time.localtime(time.time()) 
-                self.elapsed = timer() - self.disc
-                self.record(self.time_of_outage, self.elapsed)
-                
-                
-
             
         #connection fails:
         except:
@@ -53,20 +43,32 @@ class Uptimer:
                 msg = "Could not establish connecton to server, recording instance."
                 print(msg)     
                 self.disconnected = True
-                # self.recording = True
                 self.notify(msg)
+        
+        else:
+            if(self.disconnected == True):
+                self.disconnected = False
+                
+                self.time_of_outage = datetime.now().strftime("%I:%M:%S %p")
+                self.elapsed = timer() - self.disc
+                print("downtime: " + str(self.elapsed))
+                self.record(self.time_of_outage, self.elapsed)
+                
+                
+
+            
                 
 
     #writes to json
     def record(self, time, elapsed):
         print("Recording....")
         
-        date = date.today().strftime("%m/%d/%Y")
+        outage_date = datetime.now().strftime("%m/%d/%Y")
         #file write goes here
         with open('log.csv', 'w') as c:
-            writer = cvs.writer(c)
+            writer = csv.writer(c)
             print("Instance recorded")
-            writer.writerow({'Date' : date, 'Time' : time, 'Downtime' : elapsed})
+            writer.writerow([outage_date, time, elapsed])
 
 
     def init_schedule(self):
