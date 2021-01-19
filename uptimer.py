2557345aa76e31fde2f2cb48864232d3f4e6ff8d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 04:13:36 -0500

    not pretty but semi-working

--- uptimer.py
+++ uptimer.py
@@ -1,5 +1,6 @@
 from win10toast import ToastNotifier as toast
-import socket, schedule, json, time
+import socket, schedule, csv, time
+from timeit import default_timer as timer
 from datetime import date
 
 #Entire uptimer class
@@ -12,9 +13,10 @@ class Uptimer:
         self.t = toast()
         self.interval = interval
         self.disconnected = False 
+        self.recording = False
         # self.lastTime = None
         self.datetime = None
-
+      
         self.init_schedule()
 
 
@@ -25,50 +27,47 @@ class Uptimer:
             self.initial_disconnect = False
 
 
-
     #function for schedule to try connection every X amount of time
     def poll(self):
-
+        
+        self.disc = 0
         try:
             host = socket.gethostbyname('1.1.1.1')
             sock = socket.create_connection((host, 80), 2)   
             print("Ping successful")
-            if(self.diconnected):
+            if(self.disconnected == True):
+                # self.recording = False
                 self.disconnected = False
+                
+                self.time_of_outage = time.localtime(time.time()) 
+                self.elapsed = timer() - self.disc
+                self.record(self.time_of_outage, self.elapsed)
+                
+                
 
             
         #connection fails:
         except:
             if(self.disconnected == False):
+                self.disc = timer()
                 msg = "Could not establish connecton to server, recording instance."
                 print(msg)     
-                self.record()
+                self.disconnected = True
+                # self.recording = True
                 self.notify(msg)
-            self.disconnected = True
-            
-              
-
+                
 
     #writes to json
-    def record(self):
+    def record(self, time, elapsed):
+        print("Recording....")
         
-        data = json.dumps(
-                {
-                    
-                   date.today().strftime("%m/%d/%Y")  :  time.localtime(time.time()) 
-
-                }
-            )
-
+        date = date.today().strftime("%m/%d/%Y")
         #file write goes here
-        with open('log.json', 'r') as r:
-            f = r.read()
-        
-        j = json.loads(f)
-        j.update(data)
-        
-        with open('log.json', 'w') as out:
-            out.write(json.dumps(j))
+        with open('log.csv', 'w') as c:
+            writer = cvs.writer(c)
+            print("Instance recorded")
+            writer.writerow({'Date' : date, 'Time' : time, 'Downtime' : elapsed})
+
 
     def init_schedule(self):
 
@@ -78,4 +77,4 @@ class Uptimer:
             schedule.run_pending()
             time.sleep(1)
 
-Uptimer(2, False)
+Uptimer(5, False)
