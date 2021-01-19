db21c237151ae357b9f1d14a0470d19a67865dde
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 19:56:50 -0500

    class

--- conn.py
+++ conn.py
@@ -1,11 +1,59 @@
 from win10toast import ToastNotifier as toast
-import time, socket, schedule
+import socket, schedule, json, time
 
-def notify(msg1, msg2, t):
-    t.show_toast(msg1, msg2, threaded=True, icon_path=None, duration=3)
+class Uptimer:
 
-def poll():
-    try:
-        host = socket.gethostbyname('1.1.1.1')
-        sock = socket.create_connection((host, 80), 2)
-        
+    def __init__(self):
+
+        self.duration = 0
+        self.mute = False
+        self.t = ToastNotifier()
+        self.interval = 30
+        self.disconnected = False 
+        self.initial_disconnect = False
+        # self.lastTime = None
+        self.datetime = None
+        self.resumed = False
+
+    #notifies user connection is down
+    def notify(self, msg):
+        if(self.initial_disconnect):
+            if(!self.mute):
+                self.t.show_toast("Uptimer", msg, threaded=True, icon_path=None, duration=3)
+                self.initial_disconnect = False
+
+    #function for schedule to try connection every X amount of time
+    def poll(self):
+
+        try:
+            host = socket.gethostbyname('1.1.1.1')
+            sock = socket.create_connection((host, 80), 2)   
+
+            if(self.diconnected):
+                self.resumed = True
+                self.disconnected = False
+            else:
+                self.resumed = False
+
+            
+
+        except:
+            self.disconnected = True
+            self.initial_disconnect = True
+            self.msg = "Could not establish connecton to server, recording instance."
+            print(self.msg)
+            self.notify(self.msg)
+            self.record()
+
+    #writes to json
+    def record(self):
+        time = time.asctime(time.localtime(time.time()))
+
+
+    def init_schedule(interval):
+
+
+        schedule.every(30).seconds.do(self.poll())
+
+        while True:
+            schedule.run_pending()
